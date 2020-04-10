312265492318085bbc9c4032b149680be52bcbaf
SamirIngley <samir.ingle7@gmail.com>
2020-04-10 15:04:37 -0700

    checkDuplicates works

--- breakout_problems.py
+++ breakout_problems.py
@@ -28,15 +28,20 @@
 # If they are, then return true 
 # If not then False 
 
-def checkDuplicates(list): 
-	sortedList = list.sort() 
-	setList = set() 
-	for item in sortedList: 
-		setList.append(item): 
-			if item in setList:
-				return True
-			else: 
-				return False 
+# Option C:
+# sort the list, check if the current item is the same as the preivous item
+
+
+def checkDuplicates(new_list): 
+    new_list.sort() 
+    # print(new_list)
+    previous = None
+    for item in new_list:
+        # print(previous, item)
+        if previous == item:
+            return True
+        previous = item
+    return False
 
 
 # Find the middle item in a singly linked list, or two middle items if it contains an even number of nodes. 
@@ -49,39 +54,41 @@ def checkDuplicates(list):
 # Example:
 # A > B > C > D > E > F > G
 
-middle = length of the list/ 2
-if the middle is a fraction:
-	middle1 = ceiling(middle)
-	middle2 = floor(middle)
-	return middle1, middle2
-else:
-	return middle
-Code:
-def findMiddle():
-	current = self.head			# start at the head of the linked list
-	ll_length = 0
-	odd = False
-	while current:				# while a node exists in the ll
-		ll_length += 1			# add 1 to the length counter
-		current = current.next		# move on to the next node 
-	if ll_length % 2 != 0:			# if the LL is odd
-		middle = ceiling(ll_length/2)
-		odd = True
-		return middle
-	else:
-		middle = ll_length/2
-return 
+# middle = length of the list/ 2
+# if the middle is a fraction:
+# 	middle1 = ceiling(middle)
+# 	middle2 = floor(middle)
+# 	return middle1, middle2
+# else:
+# 	return middle
+# Code:
 
-index = 0
-while index != middle:
-	current = current.next
-if odd = True:
-	return current, current.next
-else:
-return current
+def findMiddle():
+    current = self.head			# start at the head of the linked list
+    ll_length = 0
+    odd = False
+    while current:				# while a node exists in the ll
+        ll_length += 1			# add 1 to the length counter
+        current = current.next		# move on to the next node 
+    if ll_length % 2 != 0:			# if the LL is odd
+        middle = ceiling(ll_length/2)
+        odd = True
+        return middle
+    else:
+        middle = ll_length/2
+
+    index = 0
+    while index != middle:
+        current = current.next
+    if odd == True:
+        return current, current.next
+    return current
 
 
 # Find the longest substring of unique letters in a given string of n letters.
 
 
 # Given a string of text and a number k, find the k words in the given text that appear most frequently. Return the words in a new array sorted in decreasing order.
+
+if __name__ == "__main__":
+    checkDuplicates([1,2,3,4,5,2])
