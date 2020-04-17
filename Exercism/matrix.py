860f53ac105f7309041c50e42dafed2e073cbc19
SamirIngley <samir.ingle7@gmail.com>
2020-04-17 16:56:56 -0700

    initial setup matrix.py

---
+++ Exercism/matrix.py
@@ -0,0 +1,35 @@
+# Exercism.io
+
+# Given a string representing a matrix of numbers, return the rows and columns 
+# of that matrix. 
+
+# 1. Generate reasonable test inputs
+# 2. Understand & solve the problem
+# 3. Simplify the problem if needed
+# 4. Find a pattern in your solution
+# 5. Make a plan - write pseudocode
+# 6. Follow your plan - write real code
+# 7. Check your work - test your code
+
+
+# INPUT: string of numbers with embedded newlines 
+# 9 8 7
+# 5 3 2
+# 6 6 7
+
+# OUTPUT: Number the rows and columns. 
+#  1  2  3
+#   |---------
+# 1 | 9  8  7
+# 2 | 5  3  2
+# 3 | 6  6  7
+
+# TESTS: 
+# 1. Normal input: small example of how it's expected to run
+# 2. Valid data types
+# 3. Boundaries
+# 4. Obeys assumptions on values
+
+# input:
+print('9 8 7\n5 3 2\n6 6 7')
+
