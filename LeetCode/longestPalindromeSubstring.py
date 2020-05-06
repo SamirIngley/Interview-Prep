bda03c706582c8fe03e33cabbcb03d5063a25a53
SamirIngley <samir.ingle7@gmail.com>
2020-05-06 03:08:09 -0700

    palindrome movement

---
+++ LeetCode/longestPalindromeSubstring.py
@@ -0,0 +1,31 @@
+# NEW SOLUTION: what we want it to do is 
+# start a loop through each char. 
+# search from the other end of the text til you find that char
+# then check in between if it's a palindrome. 
+# if not.. go on to the next letter.. if it is.. then that's the largest
+
+def palindromIterative(text, right=-1, left=0):
+
+    max_len = 0
+    curr_len = 0
+
+    for char in text:
+
+        if not char.isalpha() or not text[index].isalpha():
+            continue
+
+        # print(char_index, text[index])
+        # print('abs ', abs(index+1))
+        # print(char, text[index])
+        if len(text)//2 <= abs(index+1):
+            return True
+        
+        if char.lower() == text[index].lower():
+            curr_len += 1
+            index -= 1
+        else:
+            max_len = curr_len
+            curr_len = 0
+            index -=1
+
+    return True
